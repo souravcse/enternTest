ce44e2dc30c79de748adfd3c250894ae04a285e0
souravcse <souravcmt@gmail.com>
2026-03-08 15:32:04 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link type="text/css" rel="stylesheet" href="css.css">
+</head>
+<body>
+<div class="main">
+    <!--    Header Section-->
+    <div class="header">
+        <div class="header-top">
+            <div class="header-logo">
+                <img src="https://media.prothomalo.com/prothomalo-bangla/2025-11-17/qix5skks/palo-bangla.svg">
+            </div>
+            <div class="header-news">
+                <div class="header-news-item">
+                    <div class="header-news-item-img">
+                        <img src="https://media.prothomalo.com/prothomalo-bangla%2F2026-03-08%2Ffbiqxul2%2FKuwait-2222.PNG?rect=0%2C0%2C1600%2C1067&w=420&auto=format%2Ccompress&fmt=avif" >
+                    </div>
+                    <div class="header-news-item-content">বড় শয়তান’ যাকে অগ্রহণযোগ্য বলেছেন, তিনিই হতে যাচ্ছেন ইরানের সর্বোচ্চ নেতা</div>
+                </div>
+                <div class="header-news-item">
+                    <div class="header-news-item-img">
+                        <img src="https://media.prothomalo.com/prothomalo-bangla%2F2026-03-08%2Ffbiqxul2%2FKuwait-2222.PNG?rect=0%2C0%2C1600%2C1067&w=420&auto=format%2Ccompress&fmt=avif" >
+                    </div>
+                    <div class="header-news-item-content">বড় শয়তান’ যাকে অগ্রহণযোগ্য বলেছেন, তিনিই হতে যাচ্ছেন ইরানের সর্বোচ্চ নেতা</div>
+
+                </div>
+                <div class="header-news-item">
+                    <div class="header-news-item-img">
+                        <img src="https://media.prothomalo.com/prothomalo-bangla%2F2026-03-08%2Ffbiqxul2%2FKuwait-2222.PNG?rect=0%2C0%2C1600%2C1067&w=420&auto=format%2Ccompress&fmt=avif" >
+                    </div>
+                    <div class="header-news-item-content">বড় শয়তান’ যাকে অগ্রহণযোগ্য বলেছেন, তিনিই হতে যাচ্ছেন ইরানের সর্বোচ্চ নেতা</div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="header-bottom">
+            <ul>
+                <li><a href="#">সর্বশেষ</a> </li>
+                <li><a href="#">বাংলাদেশ</a> </li>
+                <li><a href="#">রাজনীতি</a> </li>
+                <li><a href="#">বিশ্ব</a> </li>
+                <li><a href="#">বাণিজ্য</a> </li>
+                <li><a href="#">মতামত</a> </li>
+                <li><a href="#">খেলা</a> </li>
+                <li><a href="#">বিনোদন</a> </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="content"></div>
+    <div class="footer"></div>
+</div>
+
+</body>
+</html>
